c9bdf77d4ca4812fa3831950b6a05f92a7a96c8c
TacoMarco <59195087+TacoMarco@users.noreply.github.com>
2020-06-03 21:49:23 +1000

    Add files via upload

---
+++ discord_bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="!")
+
+@client.event
+async def on_ready():
+    print('Ready')
+    await client.change_presence(activity=discord.Game(name="bread-bank.jimdosite.com"))
+
+@client.event   
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.channels, name="welcome")
+    await channel.send(f"Welcome {member.mention} to the Bread Bank server")
+    role = discord.utils.get(member.guild.roles, name="Bread Supporter")
+    await member.add_roles(role)
+    print('joined')
+
+client.run('NjMyNDczOTk2MjY4NTM1ODA4.XpZpPg.uumd2_Wxq5Ezy9Uxp-WZhyLuVrE')
